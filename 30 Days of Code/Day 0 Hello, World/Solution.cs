d02685bf32d4c88f0e655d30e408ec91416780b6
GSCVirus <gonzalo.solano@live.com>
2018-02-10 14:09:59 -0400

    Day 0 - Hello World Solution

---
+++ 30 Days of Code/Day 0 Hello, World/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day_0_Hello__World
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            String inputString; 
+        
+            inputString = Console.ReadLine(); 
+            
+            Console.WriteLine("Hello, World.");        
+            Console.WriteLine( inputString );
+        }
+    }
+}
